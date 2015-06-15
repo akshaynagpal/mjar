c3eefee800d336bea8a417cf1ddc7a8c341943ea
Akshay Nagpal <akshay2626@gmail.com>
2015-06-15 12:59:54 +0530

    changed col names

--- R/mjar.R
+++ R/mjar.R
@@ -65,7 +65,8 @@ search_MJA <-function(userQuery){
   title<-getTitles(mjaSearch)[1:10]
   link<- getLinks(mjaSearch)[1:10]
   abstract<-getAbstracts(link)
-  searchResult <- data.frame(title=title,link=link,abstract=abstract)
+  searchResult <- data.frame(title,link,abstract)
+  colnames(searchResult) <- c('title','abstract')
   #searchResult<-cbind(title,link,abstract)
   return(searchResult)
 }
